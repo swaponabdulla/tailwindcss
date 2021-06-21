224e8210ce3029e02c25b8201b27fbd90f1d94f0
swaponabdulla <77455689+swaponabdulla@users.noreply.github.com>
2021-06-21 09:25:08 +0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: { 
+      colors: {
+        
+        pink: colors,black,
+        red: colors,white,
+
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
